bb3c087f4f6b2e2c1c1021b74fd1b9cc8cbe9fdc
prakash samanta <druvils123@gmail.com>
2025-05-06 15:39:13 +0530

    updated ui for big screen

--- script.js
+++ script.js
@@ -1,12 +1,94 @@
 
-function menuTricks(){
-    let menu = document.querySelector('#menuBar')
-    let menuList = document.querySelector('.menu')
 
-    menu.addEventListener('click',function(){
-        // menuList.style.display = 'block';
-        menuList.style.backgroundColor = "red";
+// gsap.to(".img-info",{
+//   scrollTrigger: {
+//     trigger: ".page1",
+//     pin : true,
+//     // markers:true,
+//     start: "top 18%",
+//     end: "bottom bottom",
+//     endTrigger : ".last",
+//     scrub:1
+//   },
+//   y:"-510%",
+//   ease: Power1,
+// })
 
-        console.log('hello')
-    })
-}
+
+
+
+// let sections = document.querySelectorAll(".img-info")
+
+
+// Shery.imageEffect(".images",{
+//   style:3,
+//   config : { onMouse: { value:1 } },
+
+//   slideStyle: (setScroll) => {
+//      sections.forEach(function( section, index){
+//       ScrollTrigger.create({
+//         trigger: section,
+//         start: "top 50%",
+//         end: "bottom 20%",
+//         // markers:true,
+//         scrub : 1,
+//         onUpdate: function(prog) {
+//           setScroll(prog.progress+index)
+//         }
+//       })
+//      })
+//   }
+// })
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth >= 768) {
+
+    // Your GSAP + Shery code here
+ 
+
+  // --- GSAP ScrollTrigger animation ---
+  gsap.to(".img-info", {
+    scrollTrigger: {
+      trigger: ".page1",
+      pin: true,
+      // markers: true,
+      start: "top 18%",
+      end: "bottom bottom",
+      endTrigger: ".last",
+      scrub: 1
+    },
+    y: "-510%",
+    ease: Power1,
+  });
+
+  // --- Image effect with scroll syncing ---
+  let sections = document.querySelectorAll(".img-info");
+
+  Shery.imageEffect(".images", {
+    style: 3,
+    config: { onMouse: { value: 1 } },
+
+    slideStyle: (setScroll) => {
+      sections.forEach(function (section, index) {
+        ScrollTrigger.create({
+          trigger: section,
+          start: "top 50%",
+          end: "bottom 20%",
+          // markers: true,
+          scrub: 1,
+          onUpdate: function (prog) {
+            setScroll(prog.progress + index);
+          }
+        });
+      });
+    }
+  });
+ }
+});
+
+ 
+
+ 
+
